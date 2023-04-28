34ea93a90a4a02c8ff6edf9668839491caf04152
Max Dietrich <mail@mxd.codes>
2023-04-28 21:30:48 +0200

    added marker

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Map, View } from 'ol';
+import { Feature, Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import 'ol/ol.css';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Style from 'ol/style/Style';
+import Point from 'ol/geom/Point';
+import {fromLonLat} from 'ol/proj';
 
 function App() {
     const [map, setMap] = useState();
@@ -14,19 +19,37 @@ function App() {
         preload: Infinity,
         source: new OSM(),
     })
+
+    const iconFeature = new Feature({
+        geometry: new Point([0, 0]),
+        name: 'Null Island',
+        population: 4000,
+        rainfall: 500,
+      });
+      
     
+    const vectorSource = new VectorSource({
+        features: [iconFeature],
+    });
+    
+    const vectorLayer = new VectorLayer({
+        source: vectorSource,
+    })
+        
     const initialMap = new Map({
         target: mapElement.current,
-        layers: [osmLayer],
+        layers: [osmLayer, vectorLayer],
         view: new View({
             center: [0, 0],
             zoom: 0,
           }),
       });
 
+
     useEffect(() => {
         setMap(initialMap);
     }, []);
+
 
     return (
       <div style={{height:'100vh',width:'100%'}} ref={mapElement} className="map-container" />
